22fa881e94610b5b905624b6660721eca2f98886
kawlik <kpawlik1103@gmail.com>
2021-06-10 23:07:35 +0200

    Initial template

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//  component
+
+
+/*   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+const App = () => {
+
+    /*   *   *   *   *   *   *   *   *   *   */
+
+    return (
+        <>
+            Hello!
+        </>
+    );
+};
+
+/*   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+export default App;
